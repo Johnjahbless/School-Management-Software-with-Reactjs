6ec542315fb217d1138a64bf41f2d731c271071d
johnjahbless <johnjahbless@gmail.com>
2020-09-06 14:29:22 +0100

    first-commit

---
+++ build/precache-manifest.993558d07c728d02fd41335648d5cd1a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7c201e122c6f650fe41d11ab50f014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bdab49d7b67c05d6529",
+    "url": "/static/css/2.5e3a033c.chunk.css"
+  },
+  {
+    "revision": "e42241eeff63f84e7c29",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0bdab49d7b67c05d6529",
+    "url": "/static/js/2.a08c84d3.chunk.js"
+  },
+  {
+    "revision": "242f197ca750379f3ecc9fa27d681e82",
+    "url": "/static/js/2.a08c84d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e42241eeff63f84e7c29",
+    "url": "/static/js/main.93a3b897.chunk.js"
+  },
+  {
+    "revision": "02e5dc9b0a62efd85e21",
+    "url": "/static/js/runtime-main.0720b91a.js"
+  },
+  {
+    "revision": "82fa597f29de41cd41a7c402bcf09ba5",
+    "url": "/static/media/summernote.82fa597f.ttf"
+  },
+  {
+    "revision": "c1a96d26d30d9e0b2fd33c080d88c72e",
+    "url": "/static/media/summernote.c1a96d26.woff"
+  },
+  {
+    "revision": "f4a47ce92c02ef70fc848508f4cec94a",
+    "url": "/static/media/summernote.f4a47ce9.eot"
+  },
+  {
+    "revision": "f694db69cded200e4edd999fddef81b7",
+    "url": "/static/media/summernote.f694db69.woff2"
+  }
+]);
